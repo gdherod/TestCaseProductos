589bf0a02bd24bfad038f871fc43689474a89c6f
Gary Hermosilla <garuxa@gmail.com>
2020-10-17 16:06:10 -0300

    Corrección para consultar los detalles de un producto utilizando la anotacion Path

--- app/src/main/java/cl/plaplix/testcaseproductos/model/remote/ApiConsumer.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/model/remote/ApiConsumer.kt
@@ -6,13 +6,14 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FakeAPI {
     @GET("products/")
     suspend fun productsList(): Response<List<Product>>
 
-    @GET("details/")
-    suspend fun detailsProduct(): Response<List<Detail>>
+    @GET("details/{id}")
+    suspend fun detailProduct(@Path("id") id: Int): Response<List<Detail>>
 }
 
 class RetrofitClient {
