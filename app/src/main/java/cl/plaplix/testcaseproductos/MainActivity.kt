fd3220fb56633f7b0a35837f1f75e595600518bd
Gary Hermosilla <garuxa@gmail.com>
2020-10-17 12:46:33 -0300

    Mejora en el nombre de la función para obtener el listado de productos desde la API, además se agrega a la clase Repository una función para consumir desde la API el detalle de un producto

--- app/src/main/java/cl/plaplix/testcaseproductos/MainActivity.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        repository.loadApiData()
+        repository.loadApiProductData()
     }
 }
--- app/src/main/java/cl/plaplix/testcaseproductos/model/Repository.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/model/Repository.kt
@@ -10,12 +10,26 @@ class Repository {
 
     val tag = "Repository"
 
-    fun loadApiData() = CoroutineScope(Dispatchers.IO).launch {
+    fun loadApiProductData() = CoroutineScope(Dispatchers.IO).launch {
         val response = RetrofitClient.retrofitInstance().productsList()
 
         when {
             response.isSuccessful -> response.body()?.map {
                 Log.d(tag, "${it.id} - ${it.name} - ${it.price} - ${it.image}")
+            }
+            else -> Log.d(tag, response.errorBody().toString())
+        }
+    }
+
+    fun loadApiDetailData() = CoroutineScope(Dispatchers.IO).launch {
+        val response = RetrofitClient.retrofitInstance().detailsProduct()
+
+        when {
+            response.isSuccessful -> response.body()?.map {
+                Log.d(
+                    tag,
+                    "${it.id} - ${it.name} - ${it.price} - ${it.image} - ${it.description} - ${it.lastPrice} - ${it.credit}"
+                )
             }
             else -> Log.d(tag, response.errorBody().toString())
         }
