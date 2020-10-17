92d6606184dda33b176ffb83ce6d13aabfc79e9a
Gary Hermosilla <garuxa@gmail.com>
2020-10-17 12:41:13 -0300

    Probando consumo de datos desde la API utilizando la clase Repository

--- app/src/main/java/cl/plaplix/testcaseproductos/MainActivity.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/MainActivity.kt
@@ -2,10 +2,18 @@ package cl.plaplix.testcaseproductos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import cl.plaplix.testcaseproductos.model.Repository
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "MainActivity"
+
+    private var repository = Repository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repository.loadApiData()
     }
 }
