16ff3e9251990dd1b64fb99a8d214ffb03421d3e
Gary Hermosilla <garuxa@gmail.com>
2020-10-17 12:01:01 -0300

    Añadido POJOS necesarios

---
+++ app/src/main/java/cl/plaplix/testcaseproductos/model/remote/pojo/Product.kt
@@ -0,0 +1,14 @@
+package cl.plaplix.testcaseproductos.model.remote.pojo
+
+import com.google.gson.annotations.SerializedName
+
+data class Product(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("price")
+    val price: Int,
+    @SerializedName("image")
+    val image: String
+)
