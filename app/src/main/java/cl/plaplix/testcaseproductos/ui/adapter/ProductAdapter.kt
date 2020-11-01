a7da11dc2e9d1470a76be0563f4969aebf3bea0f
Gary Hermosilla <garuxa@gmail.com>
2020-11-01 20:46:31 -0300

    Creación de fragmento que contiene RecyclerView, además de crear el layout para el item del RecyclerView y clase Adaptador de la lista de productos

---
+++ app/src/main/java/cl/plaplix/testcaseproductos/ui/adapter/ProductAdapter.kt
@@ -0,0 +1,50 @@
+package cl.plaplix.testcaseproductos.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cl.plaplix.testcaseproductos.R
+import cl.plaplix.testcaseproductos.model.db.ProductEntity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import kotlinx.android.synthetic.main.product_item.view.*
+
+class ProductAdapter(private var myDataset: MutableList<ProductEntity>) :
+    RecyclerView.Adapter<ProductAdapter.ProductHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProductHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.product_item, parent, false)
+        return ProductHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        val product = myDataset[position]
+        Glide.with(holder.itemView.context)
+            .load(product.image)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(holder.productImg)
+        holder.productName.text = product.name
+        holder.productPrice.text = product.price.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return myDataset.size
+    }
+
+    class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var productName = itemView.product_name_item!!
+        var productPrice = itemView.product_price_item!!
+        var productImg = itemView.product_img_item!!
+    }
+
+    fun updateProductItems(it: List<ProductEntity>) {
+        myDataset.clear()
+        myDataset.addAll(it)
+        notifyDataSetChanged()
+    }
+
+}
