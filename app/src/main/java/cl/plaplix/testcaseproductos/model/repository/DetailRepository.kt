00abab411de31cdcebd47c3248cec46ad501421f
Gary Hermosilla <garuxa@gmail.com>
2020-11-06 23:18:47 -0300

    Añadido todo lo necesario para que el detalle de los productos tenga su propia clase Repositorio

--- app/src/main/java/cl/plaplix/testcaseproductos/model/repository/DetailRepository.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/model/repository/DetailRepository.kt
@@ -1,47 +1,50 @@
 package cl.plaplix.testcaseproductos.model.repository
 
 import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import cl.plaplix.testcaseproductos.model.db.DetailEntity
+import cl.plaplix.testcaseproductos.model.db.ProductDataBase
+import cl.plaplix.testcaseproductos.model.remote.RetrofitClient
+import cl.plaplix.testcaseproductos.model.remote.pojo.Detail
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetailRepository(context: Context) {
 
-    //TODO: Crear Detalle
-
-    /*val tag = "DetailRepository"
+    private val tag = "DetailRepository"
 
     private var productDataBase = ProductDataBase.getDataBase(context)
-    var detailList = productDataBase.productDB().getAllDetail()
 
-    fun loadApiDetailData() = CoroutineScope(Dispatchers.IO).launch {
-        val response = RetrofitClient.retrofitInstance().detailProduct()
+    fun loadApiDetailData(id: Int) = CoroutineScope(Dispatchers.IO).launch {
+        val response = RetrofitClient.retrofitInstance().detailProduct(id)
 
         when {
-            response.isSuccessful -> response.body()?.map {
-                Log.d(
-                    tag,
-                    "${it.id} - ${it.name} - ${it.price} - ${it.image} - ${it.description} - ${it.lastPrice} - ${it.credit}"
-                )
-            }
+            response.isSuccessful -> insertDetailDB(detailApiToEntity(response.body()!!))
             else -> Log.d(tag, response.errorBody().toString())
         }
     }
 
-    private fun detailApiToEntity(detailList: List<Detail>): List<DetailEntity> {
-        return detailList.map { detail ->
-            DetailEntity(
-                detail.id,
-                detail.name,
-                detail.price,
-                detail.image,
-                detail.description,
-                detail.lastPrice,
-                detail.credit
-            )
+    private fun detailApiToEntity(detailList: Detail): DetailEntity {
+        return DetailEntity(
+            detailList.id,
+            detailList.name,
+            detailList.price,
+            detailList.image,
+            detailList.description,
+            detailList.lastPrice,
+            detailList.credit
+        )
+    }
+
+    private fun insertDetailDB(detailList: DetailEntity) {
+        CoroutineScope(Dispatchers.IO).launch {
+            productDataBase.productDB().insertDetails(detailList)
         }
     }
 
-    private fun insertDetailDB(detailList: List<DetailEntity>) {
-        CoroutineScope(Dispatchers.IO).launch {
-            productDataBase.productDB().insertAllDetails(detailList)
-        }
-    }*/
+    fun getProductDetail(product_id: Int): LiveData<DetailEntity> {
+        return productDataBase.productDB().getProductDetail(product_id)
+    }
 }
