f9f16b9267f7380b727b4ec2a00ec01dd2536637
Gary Hermosilla <garuxa@gmail.com>
2020-11-06 23:22:34 -0300

    Añadido observador para navegar hacia el fragmento detalle desde el fragmento del listado de productos. Limpieza del codigo innecesario

--- app/src/main/java/cl/plaplix/testcaseproductos/ui/main/ProductListFragment.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/ui/main/ProductListFragment.kt
@@ -10,25 +10,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import cl.plaplix.testcaseproductos.R
 import cl.plaplix.testcaseproductos.model.db.ProductEntity
 import cl.plaplix.testcaseproductos.ui.adapter.ProductAdapter
+import cl.plaplix.testcaseproductos.ui.detail.DetailViewModel
+import cl.plaplix.testcaseproductos.ui.detail.ProductDetailFragment
 import kotlinx.android.synthetic.main.fragment_product_list.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProductListFragment : Fragment() {
 
     private val mainViewModel: MainViewModel by activityViewModels()
+    private val detailViewModel: DetailViewModel by activityViewModels()
     private var productList = ArrayList<ProductEntity>()
     private lateinit var productAdapter: ProductAdapter
-
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -44,20 +38,10 @@ class ProductListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product_list, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ProductListFragment().apply {
@@ -76,5 +60,18 @@ class ProductListFragment : Fragment() {
         mainViewModel.productList.observe(viewLifecycleOwner, {
             productAdapter.updateProductItems(it)
         })
+
+        productAdapter.phoneSelected.observe(viewLifecycleOwner, {
+            detailViewModel.productSelected(it)
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.mainContainer,
+                    ProductDetailFragment.newInstance("", ""),
+                    "Detail Fragment"
+                )
+                .addToBackStack("List Fragment")
+                .commit()
+        })
     }
+
 }
