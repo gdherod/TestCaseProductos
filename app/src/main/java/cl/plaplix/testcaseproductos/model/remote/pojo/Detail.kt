6167004d908b401d27eb1b7623ff26278064e041
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-26 16:59:55 -0300

    refactor: serializedname removidos por ser innecesarios

--- app/src/main/java/cl/plaplix/testcaseproductos/model/remote/pojo/Detail.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/model/remote/pojo/Detail.kt
@@ -1,20 +1,11 @@
 package cl.plaplix.testcaseproductos.model.remote.pojo
 
-import com.google.gson.annotations.SerializedName
-
 data class Detail(
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("name")
     val name: String,
-    @SerializedName("price")
     val price: Int,
-    @SerializedName("image")
     val image: String,
-    @SerializedName("description")
     val description: String,
-    @SerializedName("lastPrice")
     val lastPrice: Int,
-    @SerializedName("credit")
     val credit: Boolean
 )
--- app/src/main/java/cl/plaplix/testcaseproductos/model/remote/pojo/Product.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/model/remote/pojo/Product.kt
@@ -1,14 +1,8 @@
 package cl.plaplix.testcaseproductos.model.remote.pojo
 
-import com.google.gson.annotations.SerializedName
-
 data class Product(
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("name")
     val name: String,
-    @SerializedName("price")
     val price: Int,
-    @SerializedName("image")
     val image: String
 )
