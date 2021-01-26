e88398a1f92afd354b61e31773cbb916ccc3106a
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-26 17:14:24 -0300

    refactor: reemplazo de synthetic por viewBinding

--- app/src/main/java/cl/plaplix/testcaseproductos/ui/adapter/ProductAdapter.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/ui/adapter/ProductAdapter.kt
@@ -1,15 +1,13 @@
 package cl.plaplix.testcaseproductos.ui.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
-import cl.plaplix.testcaseproductos.R
+import cl.plaplix.testcaseproductos.databinding.ProductItemBinding
 import cl.plaplix.testcaseproductos.model.db.ProductEntity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import kotlinx.android.synthetic.main.product_item.view.*
 
 class ProductAdapter(private var myDataset: MutableList<ProductEntity>) :
     RecyclerView.Adapter<ProductAdapter.ProductHolder>() {
@@ -18,19 +16,14 @@ class ProductAdapter(private var myDataset: MutableList<ProductEntity>) :
         parent: ViewGroup,
         viewType: Int
     ): ProductHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.product_item, parent, false)
-        return ProductHolder(view)
+        val binding = ProductItemBinding.inflate(LayoutInflater.from(parent.context))
+
+        return ProductHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ProductHolder, position: Int) {
         val product = myDataset[position]
-        Glide.with(holder.itemView.context)
-            .load(product.image)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(holder.productImg)
-        holder.productName.text = product.name
-        holder.productPrice.text = product.price.toString()
+        holder.bind(product)
         holder.itemView.setOnClickListener {
             phoneSelected.value = myDataset[position]
         }
@@ -40,10 +33,16 @@ class ProductAdapter(private var myDataset: MutableList<ProductEntity>) :
         return myDataset.size
     }
 
-    class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var productName = itemView.product_name_item!!
-        var productPrice = itemView.product_price_item!!
-        var productImg = itemView.product_img_item!!
+    class ProductHolder(private val binding: ProductItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(product: ProductEntity) {
+            binding.productNameItem.text = product.name
+            binding.productPriceItem.text = product.price.toString()
+            Glide.with(binding.productImgItem.context)
+                .load(product.image)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(binding.productImgItem)
+        }
     }
 
     fun updateProductItems(it: List<ProductEntity>) {
