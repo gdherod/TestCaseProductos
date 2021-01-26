2199f323e524312299b18c6b215151986e792b49
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-26 18:04:11 -0300

    refactor: reemplazo de synthetic por viewBinding y limpieza de código innecesario

--- app/src/main/java/cl/plaplix/testcaseproductos/ui/main/ProductListFragment.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/ui/main/ProductListFragment.kt
@@ -8,55 +8,33 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import cl.plaplix.testcaseproductos.R
+import cl.plaplix.testcaseproductos.databinding.FragmentProductListBinding
 import cl.plaplix.testcaseproductos.model.db.ProductEntity
 import cl.plaplix.testcaseproductos.ui.adapter.ProductAdapter
 import cl.plaplix.testcaseproductos.ui.detail.DetailViewModel
 import cl.plaplix.testcaseproductos.ui.detail.ProductDetailFragment
-import kotlinx.android.synthetic.main.fragment_product_list.*
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class ProductListFragment : Fragment() {
 
+    private lateinit var binding: FragmentProductListBinding
     private val mainViewModel: MainViewModel by activityViewModels()
     private val detailViewModel: DetailViewModel by activityViewModels()
     private var productList = ArrayList<ProductEntity>()
     private lateinit var productAdapter: ProductAdapter
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_product_list, container, false)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    ): View {
+        binding = FragmentProductListBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         productAdapter = ProductAdapter(productList)
-        product_list.layoutManager = LinearLayoutManager(context)
-        product_list.adapter = productAdapter
+        binding.productList.layoutManager = LinearLayoutManager(context)
+        binding.productList.adapter = productAdapter
         mainViewModel.productList.observe(viewLifecycleOwner, {
             productAdapter.updateProductItems(it)
         })
