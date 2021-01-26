03cebdee289347c296ae6c79087f243b7ca26f36
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-26 16:53:04 -0300

    refactor: ahora se usa viewBinding para inflar la vista en MainActivity

--- app/src/main/java/cl/plaplix/testcaseproductos/ui/main/MainActivity.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/ui/main/MainActivity.kt
@@ -3,10 +3,12 @@ package cl.plaplix.testcaseproductos.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cl.plaplix.testcaseproductos.R
+import cl.plaplix.testcaseproductos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
