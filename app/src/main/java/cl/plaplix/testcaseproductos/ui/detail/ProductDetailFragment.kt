1ef3ebe3f72cbcc7483c195ea6682dedb9fa432e
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-26 23:35:11 -0300

    feat: agregado boton de compra para realizar acción de enviar correo para solicitar una compra de un producto

--- app/src/main/java/cl/plaplix/testcaseproductos/ui/detail/ProductDetailFragment.kt
+++ app/src/main/java/cl/plaplix/testcaseproductos/ui/detail/ProductDetailFragment.kt
@@ -1,5 +1,6 @@
 package cl.plaplix.testcaseproductos.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.activityViewModels
 import cl.plaplix.testcaseproductos.databinding.FragmentProductDetailBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.snackbar.Snackbar
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -50,6 +52,27 @@ class ProductDetailFragment : Fragment() {
                 binding.productDetailDescription.text = it.description
                 binding.productDetailLastPrice.text = it.lastPrice.toString()
                 binding.productDetailCredit.text = it.credit.toString()
+
+                fun email() {
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("info@plaplix.cl"))
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Consulta ${it.name}, id ${it.id}")
+                    intent.putExtra(
+                        Intent.EXTRA_TEXT, "Hola" +
+                                "Vi el producto ${it.name} y me gustaría que me contactaran a este correo o al " +
+                                "siguiente número _________ " +
+                                "Quedo atento."
+                    )
+                    intent.type = "message/rfc822"
+                    startActivity(Intent.createChooser(intent, "Seleccione un cliente de correo"))
+                }
+
+                binding.floatingActionButton.setOnClickListener { view ->
+                    Snackbar.make(view, "Buy!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .show()
+                    email()
+                }
             }
         })
     }
